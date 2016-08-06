678099baf224bcaf6c39d1ec2b6595e18115cf17
Michał Sikora <sikor1413@gmail.com>
2016-08-06 22:53:59 +0200

    Add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :students do
     get :subjects
   end
+  resources :teachers
+  resources :reports
+
+  get "/reports/subjects" => "reports#subject", as: "report_subjects"
+  root to: 'visitors#index'
 end
