c33fb781df392c000c878297910bd3eae4e083d2
Michał Sikora <sikor1413@gmail.com>
2016-08-08 10:06:23 +0200

    Modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@ puts "Seeds: start"
 TEACHER_TITLES = %w(Dr. Prof. TA)
 User.create!(email: 'admin@admin.com',password: 'adminadmin')
 
-30.times do
+3.times do
   Teacher.create!(
     first_name: Faker::Name.first_name,
     last_name: Faker::Name.last_name,
@@ -11,21 +11,31 @@ User.create!(email: 'admin@admin.com',password: 'adminadmin')
 end
 
 teachers = Teacher.all
-20.times do
+5.times do
   SubjectItem.create!(
     title: Faker::Lorem.sentence,
     teacher: teachers.sample
   )
 end
 
-40.times do
+25.times do
   Student.create!(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name
+    last_name: Faker::Name.last_name,
+    birthdate: Date.new([1994,1995].sample, [5,6,7,8,9,10,11].sample, [1,2,3,4].sample).to_formatted_s(:db)
   )
 end
 
 students = Student.all
+10.times do
+  StudentPayment.create!(
+    amount: Faker::Number.decimal(2),
+    due_date: Faker::Time.between(2.days.ago, DateTime.now),
+    payment_date: Faker::Time.between(10.days.ago, 2.days.ago),
+    student_id: students.sample.id
+  )
+end
+
 SubjectItem.all.each do |subject_item|
   subject_item.students << students.sample(rand(1..4))
 end
